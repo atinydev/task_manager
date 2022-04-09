a12e8a004ae6d282022638cebe28374764152112
atinydev <atinydeveloper@gmail.com>
2022-04-08 20:57:02 -0500

    created currentTaskProvider

--- lib/src/features/tasks/presentation/tasks_state.dart
+++ lib/src/features/tasks/presentation/tasks_state.dart
@@ -3,6 +3,15 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import '../domain/task.dart';
 import 'tasks_controller.dart';
 
-final tasksProvider = StateNotifierProvider<TasksNotifier, List<Task>>((ref) {
+final tasksProvider =
+    StateNotifierProvider.autoDispose<TasksNotifier, List<Task>>((ref) {
   return TasksNotifier();
+});
+
+final currentTaskProvider =
+    Provider.autoDispose.family<Task?, String?>((ref, target) {
+  if (target == null) {
+    return null;
+  }
+  return ref.watch(tasksProvider).firstWhere((task) => task.id == task.id);
 });
