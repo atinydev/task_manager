96ba265ae7dbbc2740c910a9ea710934ee995634
atinydev <atinydeveloper@gmail.com>
2022-04-08 21:41:08 -0500

    Modified task_create_edit_screen. Simple design

--- lib/src/features/tasks/presentation/task_create_edit_screen.dart
+++ lib/src/features/tasks/presentation/task_create_edit_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class TaskCreateEditScreen extends StatelessWidget {
+import 'tasks_state.dart';
+
+class TaskCreateEditScreen extends ConsumerWidget {
   const TaskCreateEditScreen({
     Key? key,
     required this.id,
@@ -11,13 +15,73 @@ class TaskCreateEditScreen extends StatelessWidget {
   final String id;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final task = ref.watch(currentTaskProvider(id));
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Name'),
+        title: (task?.title == null) ? const Text("New task") : null,
+        actions: [
+          IconButton(
+            onPressed: () {
+              if (task != null) {
+                ref.read(tasksProvider.notifier).remove(target: task);
+                context.pop();
+              }
+            },
+            icon: const Icon(
+              Icons.delete,
+            ),
+          ),
+        ],
       ),
-      body: const Center(
-        child: Text('TaskScreen'),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: Column(
+            children: const [
+              // Title
+              TextField(
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+                decoration: InputDecoration(
+                  hintText: 'Enter a title',
+                ),
+              ),
+              // Description
+              TextField(
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+                decoration: InputDecoration(
+                  icon: Icon(Icons.description),
+                  hintText: 'Description',
+                ),
+              ),
+              // Dates
+              TextField(
+                decoration: InputDecoration(
+                  icon: Icon(Icons.date_range),
+                  hintText: 'Add date/time',
+                ),
+              ),
+              // Comments
+              TextField(
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+                decoration: InputDecoration(
+                  icon: Icon(Icons.comment),
+                  hintText: 'Comments',
+                ),
+              ),
+              // Tags
+              TextField(
+                decoration: InputDecoration(
+                  icon: Icon(Icons.tag),
+                  hintText: 'Tags',
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
