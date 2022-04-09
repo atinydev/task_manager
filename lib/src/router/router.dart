b54c03c3e1eb16ff007c091bfcbba13f9f944552
atinydev <atinydeveloper@gmail.com>
2022-04-08 20:57:55 -0500

    Modified router
    Cretead a new router for TaskCreateEditScreen

--- lib/src/router/router.dart
+++ lib/src/router/router.dart
@@ -1,13 +1,25 @@
 import 'package:go_router/go_router.dart';
 
-import '../features/tasks/presentation/tasks_screen.dart';
+import '../features/tasks/presentation/task_create_edit_screen.dart';
+import '../features/tasks/presentation/tasks_list_screen.dart';
 
 final router = GoRouter(
   initialLocation: '/',
   routes: [
     GoRoute(
-      path: TasksScreen.path,
-      builder: (context, state) => const TasksScreen(),
+      path: TaskListScreen.path,
+      name: TaskListScreen.name,
+      builder: (context, state) => const TaskListScreen(),
+      routes: [
+        GoRoute(
+          path: ":id",
+          name: TaskCreateEditScreen.name,
+          builder: (context, state) {
+            final id = state.params['id']!;
+            return TaskCreateEditScreen(id: id);
+          },
+        ),
+      ],
     ),
   ],
 );
