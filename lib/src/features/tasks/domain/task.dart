4db0bd312efb00b57d74ac6a74e2ce1d5ed2c77c
atinydev <atinydeveloper@gmail.com>
2022-04-10 17:57:06 -0500

    Added serialization to task

--- lib/src/features/tasks/domain/task.dart
+++ lib/src/features/tasks/domain/task.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
+
 import 'tags.dart';
+import '../presentation/date_controller.dart';
 
 class Task {
-  final String id;
+  final int? id;
   final String title;
   final bool isComplete;
   final DateTime? date;
@@ -10,7 +13,7 @@ class Task {
   final Tags? tags;
 
   const Task({
-    required this.id,
+    this.id,
     required this.title,
     required this.isComplete,
     this.date,
@@ -19,5 +22,61 @@ class Task {
     this.tags,
   });
 
+  Map<String, dynamic> toMap() {
+    final map = {
+      'id': id,
+      'title': title,
+      'is_completed': isComplete.toInt(),
+      'due_date': date?.toDateFormat(),
+      'comments': comments,
+      'description': description,
+      'tags': tags?.toJoinByComma(),
+    };
 
+    map.removeWhere((key, value) => value == null);
+    return map;
+  }
+
+  factory Task.fromMap(Map<String, dynamic> map) {
+    final task = Task(
+      id: map['id'],
+      title: map['title'],
+      isComplete:
+          map['is_completed'] != null ? toBool(map['is_completed']) : false,
+      date: map['due_date'] != null ? DateTime.parse(map['due_date']) : null,
+      comments: map['comments'],
+      description: map['description'],
+      tags: map['tags']?.toTags(),
+    );
+    return task;
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Task.fromJson(String source) => Task.fromMap(json.decode(source));
+}
+
+extension ToInt on bool {
+  int toInt() {
+    switch (this) {
+      case true:
+        return 1;
+      default:
+        return 0;
+    }
+  }
+}
+
+bool toBool(dynamic value) {
+  if (value.runtimeType == String) {
+    value = int.parse(value);
+  }
+  switch (value) {
+    case 0:
+      return false;
+    case 1:
+      return true;
+    default:
+      throw 'Error: Convert from integer to boolean failed';
+  }
 }
