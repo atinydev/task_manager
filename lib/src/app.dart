919031efc02ba37bf18089cf6546afcf2f76d266
atinydev <atinydeveloper@gmail.com>
2022-04-10 17:55:09 -0500

    task load in app

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'router/router.dart';
 import 'theme/theme.dart';
+import 'features/tasks/presentation/task_view_controller.dart';
 
-class TaskManagerApp extends StatelessWidget {
+class TaskManagerApp extends ConsumerStatefulWidget {
   const TaskManagerApp({Key? key}) : super(key: key);
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _TaskManagerAppState();
+}
+
+class _TaskManagerAppState extends ConsumerState<TaskManagerApp> {
+  @override
+  void initState() {
+    ref.read(taskViewController).initState();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
